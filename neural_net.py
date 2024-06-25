07d948f8fdc682dac7b551d356b5ea8aa3f02bd8
devshah21 <mail2devshah@gmail.com>
2024-06-24 20:07:53 -0400

    added batch norm

--- neural_net.py
+++ neural_net.py
@@ -27,11 +27,13 @@ class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
+        self.bn1 = nn.BatchNorm1d(hidden_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, num_classes)
     
     def forward(self, x):
         out = self.fc1(x)
+        out = self.bn1(out)
         out = self.relu(out)
         out = self.fc2(out)
         return out
